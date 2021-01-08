ab9137a18077022a80e71b3e2a1f89d89944963f
Vinicius Bilck <bilck.vinicius1998@gmail.com>
2021-01-07 23:51:50 -0300

    import

--- Andromeda.py
+++ Andromeda.py
@@ -1,26 +1,25 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import numpy as np
 
 ## Declaração
-
 Num = []
 Teste = []
 i=2
 M=int(input("Número: \n"))
 
+## Verificação de números Primos 
 
-
-## Verificação de números Primos
-
-
-
-# In[26]:
-
-
-print(Teste)
+while (M>i):
+    if M%i == 0:
+        Teste.append(1)
+        i+=1
+    else:
+        i+=1
+        
+if len(Teste) == 0:
+    print("O número "+str(M)+" é primo")
+else:
+    print("O número "+str(M)+" não é primo")
+    
+##Para números grandes, um vetor grande será gerado, então posso acabar ajustar o código para que na 
+##primeira entrada de uma divisão com resto 0 inválidar. 
 
